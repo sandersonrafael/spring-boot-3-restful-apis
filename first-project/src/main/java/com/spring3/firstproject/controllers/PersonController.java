87cf0030d5ac6998118f8d7956925d88baa1d73f
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-10-30 14:00:29 -0300

    Swagger customization

--- first-project/src/main/java/com/spring3/firstproject/controllers/PersonController.java
+++ first-project/src/main/java/com/spring3/firstproject/controllers/PersonController.java
@@ -18,8 +18,16 @@ import com.spring3.firstproject.data.vo.v1.PersonVO;
 import com.spring3.firstproject.services.PersonService;
 import com.spring3.firstproject.util.MediaType;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People") // Annotation para config do Swagger
 public class PersonController {
 
     @Autowired
@@ -32,11 +40,43 @@ public class PersonController {
         produces = MediaType.APPLICATION_XML_VALUE -> determina que a aplicação serve XML
     */
     @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML } )
+    // doc swagger config
+    @Operation(
+        summary = "Finds all people", description = "Finds all people", tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))
+                )
+            }),
+            // somente @Content cria um default -> usar quando não for um retorno de VO ou DTO
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+        }
+    )
     public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML } )
+    // doc swagger config
+    @Operation(
+        summary = "Finds a person", description = "Finds a person", tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content(
+                schema = @Schema(implementation = PersonVO.class)
+            )),
+            // somente @Content cria um default -> usar quando não for um retorno de VO ou DTO
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+        }
+    )
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
@@ -44,7 +84,21 @@ public class PersonController {
     @PostMapping(
         consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
         produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }
-    ) public PersonVO create(@RequestBody PersonVO person) {
+    )
+    // doc swagger config
+    @Operation(
+        summary = "Adds a new person", description = "Adds a new person", tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content(
+                schema = @Schema(implementation = PersonVO.class)
+            )),
+            // somente @Content cria um default -> usar quando não for um retorno de VO ou DTO
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+        }
+    )
+    public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
@@ -60,11 +114,39 @@ public class PersonController {
     @PutMapping(
         consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
         produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }
-    ) public PersonVO update(@RequestBody PersonVO person) {
+    )
+    // doc swagger config
+    @Operation(
+        summary = "Updates a person", description = "Updates a person", tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Updated", responseCode = "200", content = @Content(
+                schema = @Schema(implementation = PersonVO.class)
+            )),
+            // somente @Content cria um default -> usar quando não for um retorno de VO ou DTO
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+        }
+    )
+    public PersonVO update(@RequestBody PersonVO person) {
         return service.update(person);
     }
 
     @DeleteMapping(value = "/{id}")
+    // doc swagger config
+    @Operation(
+        summary = "Deletes a person", description = "Deletes a person", tags = {"People"},
+        responses = {
+            // Como o delete não possui retorno de objeto, podemos deixar só o @Content também
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            // somente @Content cria um default -> usar quando não for um retorno de VO ou DTO
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+        }
+    )
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
